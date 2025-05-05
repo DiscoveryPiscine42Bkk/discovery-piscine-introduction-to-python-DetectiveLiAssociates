d919b359b83c814b4bacbd40a0c4af8a686e1ae5
sak- <detectiveleeassociatespi@gmail.com>
2025-05-05 16:29:01 +0700

    Create play_with_arrays.py

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+arr_0 = [2, 8, 9, 48, 8, 22, -12, 2]
+arr_1 = []
+
+for i in arr_0:
+    arr_1.append(i+2)
+
+print(f"Original array: {arr_0}")
+print(f"New array: {arr_1}")
