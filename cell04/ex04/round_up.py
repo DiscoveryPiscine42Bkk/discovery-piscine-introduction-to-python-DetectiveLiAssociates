c63da217187ca0276c154a8e2677024f75e9a5bf
sak- <detectiveleeassociatespi@gmail.com>
2025-05-05 14:04:53 +0700

    Create round_up.py

---
+++ cell04/ex04/round_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import math
+
+the_given_number = float(input("Give me a number: ").strip())
+
+round_it_up = math.ceil(the_given_number)
+
+print(round_it_up, end="\n")
+
