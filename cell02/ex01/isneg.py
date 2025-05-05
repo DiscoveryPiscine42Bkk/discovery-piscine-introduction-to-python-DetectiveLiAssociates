d4d722b1d4e6e18a1142a6fd12ee214179d0d482
sak- <detectiveleeassociatespi@gmail.com>
2025-05-05 11:40:03 +0700

    Create isneg.py

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+the_number = float(input().strip())
+if the_number > 0:
+    print("This number is positive.")
+elif the_number < 0:
+    print("This number is negative.")
+else:
+    print("This number is both positive and negative.")
