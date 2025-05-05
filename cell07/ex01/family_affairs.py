c06a34a1c436af3f07118aabe6cb0db113438c91
sak- <detectiveleeassociatespi@gmail.com>
2025-05-05 23:16:38 +0700

    Create family_affairs.py

---
+++ cell07/ex01/family_affairs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def find_the_redheads(searchHair:dict)->list:   
+    return list(map(lambda name:name.capitalize(),filter(lambda name:searchHair[name]=="red",searchHair.keys())))
+
+if __name__ == "__main__":
+
+    dupont_family={
+        "florian": "red",
+        "marie": "blond",
+        "virginie": "brunette",
+        "david": "red",
+        "franck": "red"
+    }
+
+    print(find_the_redheads(dupont_family))
+
+
+
