d40bde4ab095e238b739760a019b76466a379e87
sak- <detectiveleeassociatespi@gmail.com>
2025-05-05 22:39:57 +0700

    Create scope_that.py

---
+++ cell06/ex05/scope_that.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def add_one(varNum):
+    varNum += 1
+    return varNum
+
+if __name__ == "__main__":
+
+    testNum = 100
+    print(testNum)
+    add_one(testNum) # THE KEY to THIS PROBLEM is HERE
+    print(testNum)
