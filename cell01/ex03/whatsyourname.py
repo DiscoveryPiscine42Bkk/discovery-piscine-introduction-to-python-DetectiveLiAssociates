ad72de381c42b2294ab2e856041432bd36e6588d
sak- <detectiveleeassociatespi@gmail.com>
2025-05-05 11:28:06 +0700

    Create whatsyourname.py

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+the_first_name = input("Hey, what's your first name? : ").strip()
+the_last_name = input("And your last name? : ").strip()
+the_whole_name = the_first_name + " " + the_last_name
+
+print("Well, pleased to meet you, " + the_whole_name, end="\n")
